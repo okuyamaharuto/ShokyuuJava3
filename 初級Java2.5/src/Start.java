bc0d9a77f02ee1409d66149978ebc9d8194d80f7
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-13 11:56:28 +0900

    初級Java プロジェクトのコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,26 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		//MyFrame frame1=new MyFrame();
+		//frame1.setLocation(200,100); // ウィンドウが表示される位置
+		//frame1.setSize(150,150);
+		
+		//MyFrame frame2=new MyFrame();
+		//frame2.setLocation(350,200);
+		
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
+		
+		MyFrame frame4=new RectFrame();
+		frame4.setLocation(150,300);
+		
+		
+		int x =2;
+		int y =3;
+		System.out.println(x*y);
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
