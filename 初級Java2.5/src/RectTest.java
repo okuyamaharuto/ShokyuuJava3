6e32bcc15449491b4eb1614f6361341de3b375e0
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-14 12:18:36 +0900

    3.4　塗りつぶした楕円を描く

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -4,13 +4,17 @@ public class RectTest extends MyFrame{
     	 
     	 setColor(255,255,0); //黄色
     	 fillRect(30,40,50,100);
+    	 
     	 setColor(210,0,255); //紫
     	 fillRect(130,40,50,100);
-    	 setColor(160,160,160); //灰色
-    	 fillRect(30,200,150,10);
+    	 
     	 setColor(0,255,255); //水色
     	 fillRect(70,150,30,30);
+    	 
     	 setColor(0,0,0); //黒
     	 fillRect(110,150,30,30);
+    	 
+    	 setColor(160,160,160); //灰色
+    	 fillRect(30,200,150,10);
      }
 }
