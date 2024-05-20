1ddf49eea6e57dca2276526dd0dfd87c4570bbce
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-20 10:32:30 +0900

    練習問題3-1

---
+++ 初級Java2.5/src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame {
+	public void run() {
+		fillRect(80,60,90,50);
+		fillRect(60,100,130,30);
+		fillOval(80,130,20,20);
+		fillOval(140,130,20,20);
+	}
+	
+}
