7827730953537f2455c8b288eafb2fd8da3c3b1e
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-21 11:58:14 +0900

    5.1図形をゆっくり表示する

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 	System.out.println("プログラムを実行します");
 
-	MyFrame frame1=new RectIf();
+	MyFrame frame1=new DisplayRectsSloww();
 
 	}
 
