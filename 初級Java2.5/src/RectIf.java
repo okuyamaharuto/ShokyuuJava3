155622d7c7706e9242eb5714e0032134f51abadd
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-21 11:53:33 +0900

    追加課題その２（未完成）

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -2,13 +2,14 @@
 public class RectIf extends MyFrame {
 	public void run() {
 		int x=20;
-		int y =0;
+		int y = 120;
 	    setColor(0,128,0);
 		int i;
 		for(i=0 ; i<10 ; i++) {
-			fillRect(y,y,x,x);
-			y=y+x;
-			x*=1.4;
+			fillRect(x,50,10,y);
+			fillRect(x,300-x,10,x);
+			x+=20;
+			y-=10;
 			}
 
 		}
