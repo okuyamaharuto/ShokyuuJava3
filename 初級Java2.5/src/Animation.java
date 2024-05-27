e1931095dcbd0a74b458aa9df284fd9b669be8af
Okuyama Haruto <oku127haru0127@gmail.com>
2024-05-27 11:06:46 +0900

    練習問題5-3

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -4,13 +4,17 @@ public class Animation extends MyFrame {
 		int y = 100;
 		int x = 30;
 
-		while (y <= 200) {
+		int i;
+		for(i=0;i<3;i++) {
+			
+		
+		
+		while (x <= 170) {
 			//(1)画面を消す
 			clear();
 			//(2)四角形を表示する
 			setColor(0, 128, 0);
 			fillRect(x, y, 10, 100);
-			y += 5;
 			x+=5;
 			sleep(0.1);
 		}
@@ -23,21 +27,6 @@ public class Animation extends MyFrame {
 			sleep(0.1);
 		}
 		
-		while(y>=100) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x+=5;
-			y-=5;
-			sleep(0.1);
-		}
-		
-		while(x>=30) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x-=5;
-			sleep(0.1);
 		}
 
 	}
